c94085ed2a13129b0086b8bc7968131f40f3b8b1
Manuel Pégourié-Gonnard <mpg@elzevir.fr>
2024-03-19 22:20:13 +0100

    Resolve 1st challenge

--- src/s1c01.rs
+++ src/s1c01.rs
@@ -1,5 +1,9 @@
-pub fn base64_from_hex(hex: &str) -> Result<String, String> {
-    todo!("Convert {hex} to base64");
+use base64::prelude::*;
+use hex;
+
+pub fn base64_from_hex(hex: &str) -> Result<String, hex::FromHexError> {
+    let bytes = hex::decode(hex)?;
+    Ok(BASE64_STANDARD.encode(bytes))
 }
 
 #[cfg(test)]
@@ -9,7 +13,32 @@ mod tests {
     #[test]
     fn challenge() {
         let input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
-        let expected = String::from("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t");
+        let expected =
+            String::from("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t");
         assert_eq!(base64_from_hex(input), Ok(expected));
+    }
+
+    #[test]
+    fn errors() {
+        assert_eq!(base64_from_hex("abc"), Err(hex::FromHexError::OddLength));
+        assert_eq!(
+            base64_from_hex("abcXef"),
+            Err(hex::FromHexError::InvalidHexCharacter { c: 'X', index: 3 })
+        );
+    }
+
+    fn positive(hex: &str, exp: &str) {
+        assert_eq!(base64_from_hex(hex).unwrap(), String::from(exp));
+    }
+
+    #[test]
+    fn sizes() {
+        positive("112233445566", "ESIzRFVm");
+        positive("1122334455", "ESIzRFU=");
+        positive("11223344", "ESIzRA==");
+        positive("112233", "ESIz");
+        positive("1122", "ESI=");
+        positive("11", "EQ==");
+        positive("", "");
     }
 }
