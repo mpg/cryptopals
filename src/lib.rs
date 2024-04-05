7cd5d746918256b7881b8443c1942d383a8802dc
Manuel Pégourié-Gonnard <mpg@elzevir.fr>
2024-04-05 17:46:31 +0200

    Add 5th challenge

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,4 @@ pub mod s1c01;
 pub mod s1c02;
 pub mod s1c03;
 pub mod s1c04;
+pub mod s1c05;
---
+++ src/s1c05.rs
@@ -0,0 +1,23 @@
+pub fn rep_xor(key: &[u8], buf: &[u8]) -> Vec<u8> {
+    todo!("encrypt {:?} with repeated {:?}", buf, key);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use hex;
+
+    #[test]
+    fn challenge() {
+        let pt = b"Burning 'em, if you ain't quick and nimble\n\
+                   I go crazy when I hear a cymbal";
+        let key = b"ICE";
+        let cth = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d\
+                   63343c2a26226324272765272a282b2f20430a652e2c652a31\
+                   24333a653e2b2027630c692b20283165286326302e27282f";
+
+        let ct = hex::decode(cth).unwrap();
+
+        assert_eq!(rep_xor(key, pt), ct);
+    }
+}
