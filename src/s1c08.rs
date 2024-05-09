d05ee5f75e67c5ec7665bcfbaf21b9551f91abbf
Manuel Pégourié-Gonnard <mpg@elzevir.fr>
2024-05-09 22:43:29 +0200

    Resolve 8th challenge

--- src/s1c08.rs
+++ src/s1c08.rs
@@ -1,5 +1,19 @@
-pub fn find_ecb(cts: &[Vec<u8>]) -> usize {
-    todo!("index of ECB-looking ciphertext from {:?}", cts);
+pub fn find_ecb(cts: &[Vec<u8>]) -> Option<usize> {
+    cts.iter()
+        .enumerate()
+        .max_by_key(|(_, ct)| count_rep16(ct))
+        .map(|(i, _)| i)
+}
+
+fn block16(ct: &[u8], i: usize) -> &[u8] {
+    &ct[(16 * i)..(16 * i + 16)]
+}
+
+fn count_rep16(ct: &[u8]) -> usize {
+    (0..(ct.len() / 16))
+        .flat_map(|i| (0..i).map(move |j| (i, j)))
+        .filter(|(i, j)| block16(ct, *i) == block16(ct, *j))
+        .count()
 }
 
 #[cfg(test)]
@@ -16,10 +30,16 @@ mod tests {
     }
 
     #[test]
-    #[ignore]
     fn challenge() {
         let cts = read_lines_hex("data/08.txt");
         let idx = find_ecb(&cts);
-        assert_eq!(idx, 132);
+        assert_eq!(idx, Some(132));
+    }
+
+    #[test]
+    fn empty() {
+        let cts = Vec::new();
+        let idx = find_ecb(&cts);
+        assert_eq!(idx, None);
     }
 }
