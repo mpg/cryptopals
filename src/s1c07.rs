e952753bfcffa32c342a122c37ae6287e6036578
Manuel Pégourié-Gonnard <mpg@elzevir.fr>
2024-06-14 09:23:20 +0200

    Revisit 7th challenge: add ECB encrypt

--- src/s1c07.rs
+++ src/s1c07.rs
@@ -1,24 +1,43 @@
-use aes::cipher::{generic_array::GenericArray, BlockDecrypt, BlockSizeUser, KeyInit};
+use aes::cipher::{
+    generic_array::GenericArray, BlockDecrypt, BlockEncrypt, BlockSizeUser, KeyInit,
+};
 use aes::Aes128;
 
-pub fn aes_128_ecb_decrypt(key: &[u8], ct: &[u8]) -> Option<Vec<u8>> {
+enum Way {
+    Encrypt,
+    Decrypt,
+}
+use Way::*;
+
+fn aes_128_ecb(key: &[u8], inp: &[u8], way: Way) -> Option<Vec<u8>> {
     let cipher = Aes128::new_from_slice(key).ok()?;
 
     let block_size = Aes128::block_size();
-    if ct.len() % block_size != 0 {
+    if inp.len() % block_size != 0 {
         return None;
     }
 
-    let mut pt = Vec::new();
-    for i in (0..ct.len()).step_by(block_size) {
+    let mut out = Vec::new();
+    for i in (0..inp.len()).step_by(block_size) {
         let j = i + block_size;
-        let in_block = GenericArray::from_slice(&ct[i..j]);
+        let in_block = GenericArray::from_slice(&inp[i..j]);
         let mut out_block = Default::default();
-        cipher.decrypt_block_b2b(in_block, &mut out_block);
-        pt.extend_from_slice(out_block.as_slice());
+        match way {
+            Encrypt => cipher.encrypt_block_b2b(in_block, &mut out_block),
+            Decrypt => cipher.decrypt_block_b2b(in_block, &mut out_block),
+        }
+        out.extend_from_slice(out_block.as_slice());
     }
 
-    Some(pt)
+    Some(out)
+}
+
+pub fn aes_128_ecb_decrypt(key: &[u8], ct: &[u8]) -> Option<Vec<u8>> {
+    aes_128_ecb(key, ct, Decrypt)
+}
+
+pub fn aes_128_ecb_encrypt(key: &[u8], pt: &[u8]) -> Option<Vec<u8>> {
+    aes_128_ecb(key, pt, Encrypt)
 }
 
 #[cfg(test)]
@@ -34,7 +53,7 @@ mod tests {
     }
 
     #[test]
-    fn challenge() {
+    fn challenge_decrypt() {
         let ct = read_ct();
         let exp = std::fs::read("data/07-pt.txt").unwrap();
         assert!(ct.len() >= exp.len());
@@ -48,6 +67,17 @@ mod tests {
     }
 
     #[test]
+    fn challenge_encrypt() {
+        let pt_full = std::fs::read("data/07-pt.txt").unwrap();
+        let pt = &pt_full[0..pt_full.len() / 16 * 16];
+        let exp = &read_ct()[0..pt.len()];
+        let key = b"YELLOW SUBMARINE";
+
+        let got = aes_128_ecb_encrypt(key, pt).unwrap();
+        assert_eq!(got, exp);
+    }
+
+    #[test]
     fn bad_len() {
         let ct1 = b"WHITE SUBMARINE";
         let key = b"YELLOW SUBMARINE";
@@ -56,5 +86,10 @@ mod tests {
         assert_eq!(aes_128_ecb_decrypt(key, ct2), None);
         assert_eq!(aes_128_ecb_decrypt(ct1, key), None);
         assert_eq!(aes_128_ecb_decrypt(ct2, key), None);
+
+        assert_eq!(aes_128_ecb_encrypt(key, ct1), None);
+        assert_eq!(aes_128_ecb_encrypt(key, ct2), None);
+        assert_eq!(aes_128_ecb_encrypt(ct1, key), None);
+        assert_eq!(aes_128_ecb_encrypt(ct2, key), None);
     }
 }
